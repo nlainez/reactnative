e7c7c768a834e7c15a36067cdfd874859791c6e9
normanjoya <norman@ninjatropic.com>
2018-11-14 10:05:52 -0600

    test
    test

--- albums/index.js
+++ albums/index.js
@@ -1,7 +1,25 @@
-/** @format */
+// Import a library to help create a component
+import React from 'react';
+import { AppRegistry,
+          View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
-import { AppRegistry } from 'react-native';
-import App from './App';
-import { name as appName } from './app.json';
+// Create a component
+// Error: Unexpected block statement sorrounding arrow body
+// const App = () => {
+//   return (
+//     <Text>Some Random Text</Text>
+//   );
+// };
 
-AppRegistry.registerComponent(appName, () => App);
+// corrected form
+const App = () => (
+  <View>
+    <Header headerText={'Norman Joya!'} />
+    <AlbumList />
+  </View>
+);
+
+// render a component to the device
+AppRegistry.registerComponent('albums', () => App);
